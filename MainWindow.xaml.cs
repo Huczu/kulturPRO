691d5f5dbd576974783103402636a1df2a0db8fa
pmikolajczak <pat.mikolajczak@gmail.com>
2015-10-16 10:08:28 +0200

    Database creates when not present. Moved data getting logic to database project. Created ViewModel for adding events to model. Added AutoMapper for easy mapping of database models into ViewModels.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Database;
+using Database.Services;
+using kulturPRO.ViewModels;
+using AutoMapper;
 
 namespace kulturPRO
 {
@@ -23,6 +27,19 @@ namespace kulturPRO
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            await InitializeText();
+        }
+
+        private async Task InitializeText()
+        {
+            var userSerivce = new UserService();
+            var userViewModel = Mapper.Map<UserViewModel>(await userSerivce.GetUserById(1));
+
+            testTextBox.Text = userViewModel.FullName;
         }
     }
 }
