3e596dcd0d1bccfd72eb0b62a036cf332f552479
krystiankurylo <terrykk10@gmail.com>
2015-10-28 15:35:20 +0100

    Added interface IOnListViewModel

---
+++ KultuPRO/ViewModels/IOnListViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace KulturPRO.ViewModels
+{
+    public interface IOnListViewModel
+    {
+        ICommand SwitchViewCommand {get; set;}
+        void SwitchView();
+
+        FunctionalList FunctionalList
+        {
+            get;
+            set;
+        }
+
+    }
+}
